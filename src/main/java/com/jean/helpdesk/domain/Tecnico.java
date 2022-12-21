ca32871ef14243e3670c8d7aabfa92d2f483eaf4
JEAN MATHEUS <jean.almeida@memora.com.br>
2022-12-21 13:42:40 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/jean/helpdesk/domain/Tecnico.java
+++ src/main/java/com/jean/helpdesk/domain/Tecnico.java
@@ -1,17 +1,22 @@
 package com.jean.helpdesk.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity
 public class Tecnico extends Pessoa{
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
