abf62dee15abe6e15f7e20ed56200b7f271ef526
JEAN MATHEUS <jean.almeida@memora.com.br>
2022-12-22 08:59:41 -0300

    criando endpoint para tecnico

---
+++ src/main/java/com/jean/helpdesk/Controller/TecnicoController.java
@@ -0,0 +1,23 @@
+package com.jean.helpdesk.Controller;
+
+import com.jean.helpdesk.domain.Tecnico;
+import com.jean.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tecnicos")
+public class TecnicoController {
+    @Autowired
+    private TecnicoService tecnicoService;
+    @GetMapping("/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+        Tecnico obj = tecnicoService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/jean/helpdesk/services/TecnicoService.java
@@ -0,0 +1,19 @@
+package com.jean.helpdesk.services;
+
+import com.jean.helpdesk.domain.Tecnico;
+import com.jean.helpdesk.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
